fd5129d3331b6e9f72945d43fbfe752cb31d94e5
Kiyotan <appleumasugi.mjd@gmail.com>
2019-05-05 14:48:30 +0900

    Initial Commit

---
+++ Like-InstaApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Like-InstaApp
+//
+//  Created by Kiyoto Ryuman on 2019/05/05.
+//  Copyright © 2019 Kiyoto Ryuman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
